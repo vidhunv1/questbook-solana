6125dc91cb7098a64a03632554d101556f23609c
hadi <hadi.mohammad.saleh1@gmail.com>
2022-03-25 16:48:24 +0300

    init frontend

---
+++ app/pages/index.tsx
@@ -0,0 +1,9 @@
+import ConnectToPhantom from "../components/ConnectToPhantom";
+
+export default function Home() {
+  return (
+    <div className="">
+      <ConnectToPhantom />
+    </div>
+  );
+}
