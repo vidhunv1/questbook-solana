f3a5a7a609152742c4bbc8aa39aea7384e3f615a
hadi <hadi.mohammad.saleh1@gmail.com>
2022-03-29 15:22:21 +0300

    add wallet modals and remove the dependency on anchor's provider

---
+++ app/pages/workspaces/CreateWorkspace.tsx
@@ -0,0 +1,12 @@
+import rpcCreateWorkspace from "../../lib/questbook-solana";
+import ConnectToPhantom from "../../src/components/ConnectToPhantom"
+
+const CreateWorkSpace = () => {
+  return (
+    <div>
+      <ConnectToPhantom/>
+    </div>
+  );
+};
+
+export default CreateWorkSpace;
